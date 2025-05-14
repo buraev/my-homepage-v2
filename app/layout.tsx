11ae4faf37832add3e55bbe4ec8ce87e0b075421
buraev <buraevvasilii@gmail.com>
2025-05-14 13:19:59 +0300

    change font

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata: Metadata = {
 }
 
 const mainFont = localFont({
-  src: "../src/shared/assets/fonts/Martian_Mono/static/MartianMono-Light.ttf",
+  src: "../src/shared/assets/fonts/JetBrainsMono-Regular.woff2",
   display: "swap",
 })
 
